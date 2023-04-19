93cb301ada8d674c890872125e78281606a20968
Ecem <51851879+ecemisildar@users.noreply.github.com>
2023-04-19 15:38:16 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./compile -Xlint
-#-planner opt-blind
-#java -jar enhsp-dist/enhsp.jar -o robcoffee.pddl -f robcoffee-prob1.pddl 
-#java -jar enhsp-dist/enhsp.jar -o robcoffee.pddl -f robcoffee-prob2.pddl opt-blind
-#java -jar enhsp-dist/enhsp.jar -o robcoffee.pddl -f robcoffee-prob3.pddl 
-java -jar enhsp-dist/enhsp.jar -o robcoffee.pddl -f robcoffee-prob4.pddl opt-blind
-
